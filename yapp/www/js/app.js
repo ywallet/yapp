3b8ca311f7db120624e0e4554f14b92138715134
fjnogueira <fjnogueira@yahoo.com>
2014-11-26 13:51:05 +0000

    refactoring initial template do make it compliant with the guidelines found in https://github.com/ywallet/angularjs-styleguide

--- yapp/www/js/app.js
+++ yapp/www/js/app.js
@@ -5,8 +5,10 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers'])
+       .run(runFunction)
+       .config(configFunction);
 
-.run(function($ionicPlatform) {
+function runFunction($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -18,55 +20,15 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       StatusBar.styleDefault();
     }
   });
-})
+}
 
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-    .state('app', {
-      url: "/app",
-      abstract: true,
-      templateUrl: "templates/menu.html",
-      controller: 'AppCtrl'
-    })
-
-    .state('app.search', {
-      url: "/search",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/search.html"
-        }
-      }
-    })
-
-    .state('app.browse', {
-      url: "/browse",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/browse.html"
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: "/playlists",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-    .state('app.single', {
-      url: "/playlists/:playlistId",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/playlist.html",
-          controller: 'PlaylistCtrl'
-        }
-      }
-    });
+function configFunction($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('app'            , { url: "/app"     , abstract: true, templateUrl: "templates/menu.html", controller: 'AppCtrl' })
+                .state('app.search'     , { url: "/search"                  , views: { 'menuContent' : { templateUrl: "templates/search.html" }}})
+                .state('app.browse'     , { url: "/browse"                  , views: { 'menuContent' : { templateUrl: "templates/browse.html" }}})
+                .state('app.playlists'  , { url: "/playlists"               , views: { 'menuContent' : { templateUrl: "templates/playlists.html", controller: 'PlaylistsCtrl' }}})
+                .state('app.single'     , { url: "/playlists/:playlistId"   , views: { 'menuContent' : { templateUrl: "templates/playlist.html", controller: 'PlaylistCtrl' }}});
+    
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/playlists');
-});
-
+}
