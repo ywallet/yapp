443327e5c04544520e55e8797c1043cd1fbc9b34
git-afsantos <contact.andre.santos@gmail.com>
2014-12-03 15:02:24 +0000

    Added IIFE to enclose controllers.js.

--- yapp/www/js/controllers.js
+++ yapp/www/js/controllers.js
@@ -1,47 +1,49 @@
-angular.module('starter.controllers', [])
-       .controller('AppCtrl', AppCtrlFunction )
-       .controller('PlaylistsCtrl', PlaylistsCtrlFunction)
-       .controller('PlaylistCtrl', PlaylistCtrlFunction)
+(function () {
+    angular.module('starter.controllers', [])
+           .controller('AppCtrl', AppCtrlFunction )
+           .controller('PlaylistsCtrl', PlaylistsCtrlFunction)
+           .controller('PlaylistCtrl', PlaylistCtrlFunction)
 
-PlaylistCtrlFunction.$inject = ['$scope', '$stateParams'];
-function PlaylistCtrlFunction($scope, $stateParams) {
-};
+    PlaylistCtrlFunction.$inject = ['$scope', '$stateParams'];
+    function PlaylistCtrlFunction($scope, $stateParams) {
+    };
 
-PlaylistsCtrlFunction.$inject = ['$scope'];
-function PlaylistsCtrlFunction($scope) {
-  $scope.playlists = [
-    { title: 'Reggae'   , id: 1 },
-    { title: 'Chill'    , id: 2 },
-    { title: 'Dubstep'  , id: 3 },
-    { title: 'Indie'    , id: 4 },
-    { title: 'Rap'      , id: 5 },
-    { title: 'Cowbell'  , id: 6 }
-  ];
-}
+    PlaylistsCtrlFunction.$inject = ['$scope'];
+    function PlaylistsCtrlFunction($scope) {
+      $scope.playlists = [
+        { title: 'Reggae'   , id: 1 },
+        { title: 'Chill'    , id: 2 },
+        { title: 'Dubstep'  , id: 3 },
+        { title: 'Indie'    , id: 4 },
+        { title: 'Rap'      , id: 5 },
+        { title: 'Cowbell'  , id: 6 }
+      ];
+    }
 
-AppCtrlFunction.$inject = ['$scope', '$ionicModal', '$timeout'];
-function AppCtrlFunction($scope, $ionicModal, $timeout) {
-  // Form data for the login modal
-  $scope.loginData = {};
+    AppCtrlFunction.$inject = ['$scope', '$ionicModal', '$timeout'];
+    function AppCtrlFunction($scope, $ionicModal, $timeout) {
+      // Form data for the login modal
+      $scope.loginData = {};
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', { scope: $scope })
-             .then(function(modal) { $scope.modal = modal; });
+      // Create the login modal that we will use later
+      $ionicModal.fromTemplateUrl('templates/login.html', { scope: $scope })
+                 .then(function(modal) { $scope.modal = modal; });
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() { $scope.modal.hide(); };
+      // Triggered in the login modal to close it
+      $scope.closeLogin = function() { $scope.modal.hide(); };
 
-  // Open the login modal
-  $scope.login = function() { $scope.modal.show(); };
+      // Open the login modal
+      $scope.login = function() { $scope.modal.show(); };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+      // Perform the login action when the user submits the login form
+      $scope.doLogin = function() {
+        console.log('Doing login', $scope.loginData);
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-}
+        // Simulate a login delay. Remove this and replace with your login
+        // code if using a login system
+        $timeout(function() {
+          $scope.closeLogin();
+        }, 1000);
+      };
+    }
+})();
