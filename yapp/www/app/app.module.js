1b61829457b37d084ba0bbadb42371ef2355dead
git-afsantos <contact.andre.santos@gmail.com>
2014-12-10 17:02:18 +0000

    Removed duplicate files to conform to new directory structure.

--- yapp/www/app/app.module.js
+++
@@ -1,2 +0,0 @@
-// Ionic Starter App
-angular.module("yapp", ["ionic", "yapp.controllers", "yapp.allowances", "yapp.dashboard", "yapp.history", "yapp.notifications", "yapp.payments", "yapp.savings", "yapp.authentication"]);
--- yapp/www/app/history/history.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    angular.module('yapp.history', [])
-           .controller('History', History)
-    
-    History.$inject = ['$scope'];
-    function History($scope) {
-      $scope.mesada = {};
-    }
-})();
