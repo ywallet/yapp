7acf1f9898d2bfb00b32d01bace74c3ec2a6badf
git-afsantos <contact.andre.santos@gmail.com>
2014-12-10 22:41:16 +0000

    Cosmetic changes and template code for log in and registration.

--- yapp/www/yapp/authentication/authentication.js
+++ yapp/www/yapp/authentication/authentication.js
@@ -20,6 +20,7 @@
         vm.state = "home";
 
         vm.doLogin = doLogin;
+        vm.doRegister = doRegister;
         vm.goToRegistration = goToRegistration;
         vm.goToLogin = goToLogin;
 
@@ -30,7 +31,15 @@
             vm.user.loginData.username = "";
             vm.user.pass = vm.user.loginData.password;
             vm.user.loginData.password = "";
-            vm.state = "home";
+            // vm.state = "home";
+        }
+
+        function doRegister() {
+            vm.user.name = vm.user.loginData.username;
+            vm.user.loginData.username = "";
+            vm.user.pass = vm.user.loginData.password;
+            vm.user.loginData.password = "";
+            // vm.state = "home";
         }
 
         function goToRegistration() {
