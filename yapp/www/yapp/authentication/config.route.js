4b5a596333771cc54afaf63ac2928a081bec0a98
git-afsantos <contact.andre.santos@gmail.com>
2014-12-12 18:09:43 +0000

    Split the authentication page into three subviews.

--- yapp/www/yapp/authentication/config.route.js
+++ yapp/www/yapp/authentication/config.route.js
@@ -8,12 +8,39 @@
     defineRoutes.$inject = ["$stateProvider"];
 
     function defineRoutes($stateProvider) {
-        $stateProvider.state("yapp.authentication", {
+        $stateProvider.state("authentication", {
             url: "/authentication",
+            abstract: true,
+            templateUrl: "yapp/authentication/authentication.html"
+        }).state("authentication.index", {
+            url: "",
+            views: {
+                authContent: {
+                    templateUrl: "yapp/authentication/authentication-index.html"
+                }
+            }
+        }).state("authentication.signin", {
+            url: "/signin",
+            views: {
+                authContent: {
+                    templateUrl: "yapp/authentication/signin.html",
+                    controller: "Authentication"
+                }
+            }
+        }).state("authentication.register", {
+            url: "/register",
+            views: {
+                authContent: {
+                    templateUrl: "yapp/authentication/register.html",
+                    controller: "Authentication"
+                }
+            }
+        }).state("yapp.signout", {
+            url: "/signout",
             views: {
                 menuContent: {
-                    templateUrl: "yapp/authentication/authentication.html",
-                    controller: "Authentication"
+                    templateUrl: "yapp/authentication/signout.html",
+                    controller: "SignOut"
                 }
             }
         });    	
