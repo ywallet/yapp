6cd5a71e8c7091396350426ff5913589e22fbc5c
git-afsantos <contact.andre.santos@gmail.com>
2015-01-21 23:07:56 +0000

    Added sign in http request.

--- yapp/www/yapp/authentication/signin.js
+++ yapp/www/yapp/authentication/signin.js
@@ -5,9 +5,9 @@
         .module("yapp.authentication")
         .controller("SignIn", SignIn);
 
-    SignIn.$inject = ["$scope", "StateRouter", "DSUser"];
+    SignIn.$inject = ["$scope", "$http", "StateRouter", "DSUser"];
 
-    function SignIn($scope, StateRouter, DSUser) {
+    function SignIn($scope, $http, StateRouter, DSUser) {
         $scope.signinData = {
             email: "",
             password: ""
@@ -18,12 +18,32 @@
         ////////////////////
 
         function doSignIn() {
+            var data = {
+                email: $scope.signinData.email,
+                password: $scope.signinData.password
+            };
             $scope.signinData.password = "";
+            $http.post("https://ywallet.herokuapp.com/managers", data)
+                .success(onSignInSuccess)
+                .error(onSignInError);
+        }
+
+
+        function onSignInSuccess(data, status, headers, config) {
             DSUser.putUser({
                 name: "yUser",
                 email: $scope.signinData.email
             });
             StateRouter.goAndForget("yapp.dashboard");
+        }
+
+        function onSignInError(data, status, headers, config) {
+            if (data && data.errors) {
+                console.error("error authenticating", data.errors);
+            } else {
+                // TODO development only
+                onSignInSuccess(data, status, headers, config);
+            }
         }
     }
 })();
