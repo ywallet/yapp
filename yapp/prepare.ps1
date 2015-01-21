78b58b4c31048abefc41f8e77bd3a6ab4edb18cb
fjnogueira <fjnogueira@yahoo.com>
2015-01-21 18:39:39 +0000

    added prepare scripts and added barcode scanner to scan and generate qrcodes

--- yapp/prepare.ps1
+++ yapp/prepare.ps1
@@ -3,3 +3,6 @@ ionic platform add wp8
 ionic plugin add org.apache.cordova.inappbrowser
 ionic plugin add org.apache.cordova.splashscreen
 ionic plugin add com.ionic.keyboard
+ionic plugin add org.apache.cordova.crashlytics
+ionic plugin add https://github.com/wildabeast/BarcodeScanner.git
+
