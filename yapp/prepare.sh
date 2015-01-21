9e600ed68a29c1796d2416f945c34f28c4780d91
fjnogueira <fjnogueira@yahoo.com>
2015-01-21 15:31:45 +0000

    added prepare scripts

---
+++ yapp/prepare.sh
@@ -0,0 +1,5 @@
+ionic platform add android
+ionic platform add wp8
+ionic plugin add org.apache.cordova.inappbrowser
+ionic plugin add org.apache.cordova.splashscreen
+ionic plugin add com.ionic.keyboard
